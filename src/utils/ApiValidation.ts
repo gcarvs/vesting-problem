eccec6a2092a86b37275abfc62580e4a34f90e84
Gabriel de Carvalho dos Santos <gabriel.infosantos@gmail.com>
2025-11-02 16:22:29 -0300

    feat: created validation functions for user input data

---
+++ src/utils/ApiValidation.ts
@@ -0,0 +1,28 @@
+import * as path from "path";
+
+/**
+ * Tests if a provided string is a valid file name as contains only allowed extensions.
+ * 
+ * @param {string} fileName - The string to be tested
+ * @returns {boolean} - True if the string is a valid filename or false if not
+ */
+export function validateFileName(fileName: string): boolean {
+    const rgxForbiddenChars = /^[^\\/:\*\?"<>\|]+$/; // forbidden characters \ / : * ? " < > |
+    const allowedExtensions = ['.csv'];
+
+    if(!rgxForbiddenChars.test(fileName)) return false;
+
+    const fileExtension = path.extname(fileName).toLowerCase(); // Get extension and convert to lowercase 
+    if(!allowedExtensions.includes(fileExtension)) return false;
+    return true;
+}
+
+/**
+ * Tests if a possible date string follows the format YYYY-MM-DD
+ * @param {string} dateString - The string to b tested
+ * @returns {boolean} - True if the string is a valid date or false if not
+ */
+export function validateTargetDate(dateString: string): boolean {
+    const dateFormatRegex = /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/;
+    return dateFormatRegex.test(dateString);
+}
---
+++ tests/utils/ApiValidation.test.ts
@@ -0,0 +1,41 @@
+import { validateFileName, validateTargetDate } from "../../src/utils/ApiValidation";
+
+describe("test API Validations", () => {
+    describe("test validateFileName function", () => {
+        test("test multiple scenarios", () => {
+            const invalidString1: string = "invalid/filename.csv";
+            const invalidString2: string = "invalid*file.csv";
+            const invalidString3: string = "invalid\"file.csv";
+            const invalidString4: string = "my_valid_filename.pdf";
+
+            const validString:string = "my_valid_filename.csv";
+
+            expect(validateFileName(invalidString1)).toBeFalsy();
+            expect(validateFileName(invalidString2)).toBeFalsy();
+            expect(validateFileName(invalidString3)).toBeFalsy();
+            expect(validateFileName(invalidString4)).toBeFalsy();
+
+            expect(validateFileName(validString)).toBeTruthy();
+        });
+    });
+
+    describe("test validateTargetDate", () => {
+        describe("test multiple scenarios", () => {
+            const invalidString1: string = "10-06-2025";
+            const invalidString2: string = "10/06/2025";
+            const invalidString3: string = "2025/06/10";
+            const invalidString4: string = "June 10th, 2025";
+            const invalidString5: string = "Not a Date";
+
+            const validString:string = "2025-06-10";
+
+            expect(validateTargetDate(invalidString1)).toBeFalsy();
+            expect(validateTargetDate(invalidString2)).toBeFalsy();
+            expect(validateTargetDate(invalidString3)).toBeFalsy();
+            expect(validateTargetDate(invalidString4)).toBeFalsy();
+            expect(validateTargetDate(invalidString5)).toBeFalsy();
+
+            expect(validateTargetDate(validString)).toBeTruthy();
+        }); 
+    });
+});
